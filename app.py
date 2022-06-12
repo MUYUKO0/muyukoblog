1014c401b18b25f4f01f6460e88660a5cf915d79
MuhammetFurkan24 <94463251+MuhammetFurkan24@users.noreply.github.com>
2022-06-13 02:39:34 +0300

    project update

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 # Muyuko web page - Flask app
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+konu = ["Minecraft", "Youtube", "Python", "Java"]  # örnek
+
+
+@app.route("/")
+def main():
+    return render_template("index.html", konu=konu)
 
 
 if __name__ == "__main__":
